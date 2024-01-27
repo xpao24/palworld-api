4d2d9e6e463cfc2efe977ca4914208e962705f40
lucas <startupfa.st2023@gmail.com>
2024-01-27 14:25:41 +0800

    port

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@ const app = new Elysia()
       query: queryIndexPals,
     }
   )
-  .listen(3000);
+  .listen(Bun.env.PORT ?? 3000);
 
 console.log(`🦊 Elysia is running at on port ${app.server?.port}...`);
